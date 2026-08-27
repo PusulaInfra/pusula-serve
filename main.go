979ed670baf18b9906a8646cff90807710cfe4f0
ccyhun67-gif <ccyhun.67@gmail.com>
2026-08-27 14:06:27 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
 func main() {
-	cfg := ServingConfig{
-		ModelName:            "meta-llama/Llama-3-70b-Instruct",
-		MaxModelLen:          65536,
-		GpuMemoryUtilization: 0.90,
-		MaxNumSeqs:           256,
-	}
-
-	AnalyzeConfig(cfg)
+	StartServer()
 }
